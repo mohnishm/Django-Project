39a08df492f1dffb7606c52b8e4948301e8537aa
Mohnish Mallya <mohnish.mallya@mountblue.io>
2019-01-14 14:46:50 +0530

    Minor Refactor

--- ipl/iplapi/views.py
+++ ipl/iplapi/views.py
@@ -21,12 +21,7 @@ def matches_per_season_charts(request):
     all_matches = Matches.objects.all()
     count = Counter([match.season for match in all_matches])
     context = {"matches_per_season": count}
-    data = {
-        "matches_data":dict(context),
-        "title":"Matches Per Season",
-        "text":"Number of Matches",
-        "bottom":"Seasons"
-    }
+    data = dict(context)
     json_data = json.dumps(data)
     return render(request, "iplapi/matches.html", context={"data":json_data})
 
@@ -48,12 +43,7 @@ def wins_per_season_charts(request):
     for data in winners_in_season:
         if data["season"] in context and data["winner"] != "":
             context[data["season"]].append({"team":data["winner"],"total_wins":data["winner__count"]})
-    data = {
-        "matches_data":dict(context),
-        "title":"Wins per team for every season ",
-        "text":"Number of wins",
-        "bottom":"Seasons"
-    }
+    data = dict(context)
     json_data = json.dumps(data)
     return render(request, "iplapi/winsperseason.html", context={"data":json_data})
 
@@ -68,12 +58,7 @@ def extra_runs(request):
 def extra_runs_charts(request):
     extraruns_by_team = Matches.objects.filter(season=2016).annotate(bowling_team=F("deliveries__bowling_team")).values("bowling_team").annotate(extra_runs=Sum("deliveries__extra_runs"))
     context = {"extra_runs_2016":list(extraruns_by_team)}
-    data = {
-        "matches_data":dict(context),
-        "title":"Extra runs conceded by each team in 2016",
-        "text":"Extra runs",
-        "bottom":"teams"
-    }
+    data = dict(context)
     json_data = json.dumps(data)
     return render(request, "iplapi/extraruns.html", context={"data":json_data})
 
@@ -90,12 +75,7 @@ def economy_charts(request):
     top_economical_bowler = Matches.objects.filter(season=2015).annotate(bowler=F("deliveries__bowler")).values("bowler").annotate(economy=ExpressionWrapper(Sum("deliveries__total_runs")/(Count("deliveries__ball")/6), output_field=DecimalField())).order_by("economy")[:5]
     top_economical_bowler = [{"bowler":query["bowler"], "economy":round(float(query["economy"]),2)} for query in top_economical_bowler]
     context = {"top_economical_bowler":list(top_economical_bowler)}
-    data = {
-        "matches_data":dict(context),
-        "title":"Top economical bowlers of 2015",
-        "text":"Economy",
-        "bottom":"Bowlers"
-    }
+    data = dict(context)
     json_data = json.dumps(data)
     return render(request, "iplapi/economy.html", context={"data":json_data})
 
@@ -112,12 +92,7 @@ def batting_average_charts(request):
     top_batsmen = Matches.objects.filter(season=2012).annotate(batsman=F("deliveries__batsman")).values("batsman").annotate(batsman_avg=ExpressionWrapper(Sum("deliveries__batsman_runs")/Count(F("deliveries__match_id"), distinct=True),output_field=FloatField())).order_by("-batsman_avg")[:5]
     top_average = [{"batsman": query["batsman"], "average": round(query["batsman_avg"], 2)} for query in top_batsmen]
     context = {"top_batting_average": top_average}
-    data = {
-        "matches_data":dict(context),
-        "title":"Top Batting Average for 2012",
-        "text":"Batting Average",
-        "bottom":"Batsman"
-    }
+    data = dict(context)
     json_data = json.dumps(data)
     return render(request, "iplapi/batting_avg.html", context={"data":json_data})
 
